3925b29a8e46151c52bacfce9c1631acc6cc0479
Nurzhan <nurzhait@gmail.com>
2024-09-02 10:32:41 +0500

    Add project files.

---
+++ Tgha/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Telegram.Bot;
+
+Console.WriteLine("Welcome to Telegram Poster");
+
+if (args.Length < 2) throw new ArgumentException("Please supply arguments.");
+
+var token = args[0];
+var chatIdString = args[1];
+
+
+var tryParseResult = long.TryParse(chatIdString, out var chatId);
+
+if (!tryParseResult)
+{
+    throw new ArgumentException("Please supply chatId.");
+}
+
+if (chatId > 0)
+{
+    throw new ArgumentException("Please supply chatId.");
+}
+
+var bot = new TelegramBotClient(token);
+
+await bot.SendTextMessageAsync(chatId, "Halo!");
